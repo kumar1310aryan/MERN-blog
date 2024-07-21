9e95bcea5f382d6de377313a3266fefa9b9fecc2
kumar1310aryan <kumar1310aryan@gmail.com>
2024-07-21 14:07:53 +0530

    blog app

---
+++ src/pages/Home.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        Habibi, welcome to Dubai
+      </h1>
+      <p className="mx-auto leading-relax text-base mb-4">
+        We highly recommend using Vite, Parcel, Next.js, or Remix instead of
+        Create React App. They provide an equivalent or better developer
+        experience but with more flexibility, giving you more control over how
+        Tailwind and PostCSS are configured.Create your projectStart by creating
+        a new React project with Create React App v5.0+ if you don't have one
+        already set up.
+      </p>
+      <p className="mx-auto leading-relax text-base mb-4">
+        We highly recommend using Vite, Parcel, Next.js, or Remix instead of
+        Create React App. They provide an equivalent or better developer
+        experience but with more flexibility, giving you more control over how
+        Tailwind and PostCSS are configured.Create your projectStart by creating
+        a new React project with Create React App v5.0+ if you don't have one
+        already set up.
+      </p>
+      <p className="mx-auto leading-relax text-base mb-4">
+        We highly recommend using Vite, Parcel, Next.js, or Remix instead of
+        Create React App. They provide an equivalent or better developer
+        experience but with more flexibility, giving you more control over how
+        Tailwind and PostCSS are configured.Create your projectStart by creating
+        a new React project with Create React App v5.0+ if you don't have one
+        already set up.
+      </p>
+      <p className="mx-auto leading-relax text-base mb-4">
+        We highly recommend using Vite, Parcel, Next.js, or Remix instead of
+        Create React App. They provide an equivalent or better developer
+        experience but with more flexibility, giving you more control over how
+        Tailwind and PostCSS are configured.Create your projectStart by creating
+        a new React project with Create React App v5.0+ if you don't have one
+        already set up.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
