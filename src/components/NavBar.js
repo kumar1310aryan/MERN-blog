d3540adad703dc8363b3c8ef255561e1bf7fb92e
kumar1310aryan <kumar1310aryan@gmail.com>
2024-07-22 17:26:10 +0530

    NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,27 +3,31 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="border-b-4 border-green-700 text-center fixed top-0 bg-green-600 font-bold w-full text-lg text-white">
-      <ul>
-        <li className="inline-block py-4">
-          <Link to="/" className="pl-6 pr-8">
+    <nav className="bg-gray-900 text-center fixed top-0 w-full text-white shadow-lg font-serif">
+      <ul className="flex justify-center space-x-8 py-4">
+        <li className="relative group">
+          <Link to="/" className="px-4 py-2 text-lg tracking-wide transition-colors duration-300 group-hover:text-cyan-400">
             Home
           </Link>
+          <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-cyan-400 scale-x-0 group-hover:scale-x-90 transition-transform duration-300"></span>
         </li>
-        <li className="inline-block py-4">
-          <Link to="/about" className="pl-6 pr-8">
+        <li className="relative group">
+          <Link to="/about" className="px-4 py-2 text-lg tracking-wide transition-colors duration-300 group-hover:text-cyan-400">
             About
           </Link>
+          <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-cyan-400 scale-x-0 group-hover:scale-x-90 transition-transform duration-300"></span>
         </li>
-        <li className="inline-block py-4">
-          <Link to="/article" className="pl-6 pr-8">
+        <li className="relative group">
+          <Link to="/article" className="px-4 py-2 text-lg tracking-wide transition-colors duration-300 group-hover:text-cyan-400">
             Article
           </Link>
+          <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-cyan-400 scale-x-0 group-hover:scale-x-90 transition-transform duration-300"></span>
         </li>
-        <li className="inline-block py-4">
-          <Link to="/articles-list" className="pl-6 pr-8">
+        <li className="relative group">
+          <Link to="/articles-list" className="px-4 py-2 text-lg tracking-wide transition-colors duration-300 group-hover:text-cyan-400">
             Articles List
           </Link>
+          <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-cyan-400 scale-x-0 group-hover:scale-x-90 transition-transform duration-300"></span>
         </li>
       </ul>
     </nav>
