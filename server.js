70efe6ea795c7c55b0d7c85bdf070972a56b71f2
kumar1310aryan <kumar1310aryan@gmail.com>
2024-07-24 15:05:06 +0530

    database connectiion

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const app = express();
+const { MongoClient } = require("mongodb");
 const PORT = process.env.PORT || 8000;
-
-const articlesInfo = {
-  "learn-react": {
-    comments: [],
-  },
-  "learn-node": {
-    comments: [],
-  },
-  "my-thoughts-on-learning-react": {
-    comments: [],
-  },
-};
 
 //initialize middleware
 //we use to have to install body parser but now it is a buildt in middleware
 //function of express. It parses incoming JSON payload
 app.use(express.json({ extended: false }));
+
+app.get("/api/articles/:name", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+    const client = await MongoClient.connect("mongodb://localhost:27017");
+    const db = client.db("mernBlog");
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to database", error });
+  }
+});
 
 app.get("/", (req, res) => res.send("hello world"));
 
